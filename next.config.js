293757c7538917cbb024b7a692852286b28ab9c6
abrnsyh <abriansyah1099@gmail.com>
2022-09-29 19:42:42 +0700

    Ready for Github Pages

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	basePath: !debug ? "/introduction-to-react-nextjs" : "",
-	assetPrefix: !debug ? "/introduction-to-react-nextjs/" : "",
+	basePath: "/introduction-to-react-nextjs",
+	assetPrefix: "/introduction-to-react-nextjs/",
 };
 
 module.exports = nextConfig;
